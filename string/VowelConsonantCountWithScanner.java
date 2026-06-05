ddd74370d43b24e0c6d6cf6dd21400f30c1d81c6
devprajwalgotnochill <devprajwolgotnochill@gmail.com>
2026-06-05 08:57:03 +0545

    str

---
+++ string/VowelConsonantCountWithScanner.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class VowelConsonantCountWithScanner {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in)
+        System.out.println("Enter a sentence");
+        String str = sc.nextLine();
+
+        int vowels = 0;
+        int consonants = 0;
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            //check if char is a letter 
+            if ((ch >= 'a' && ch <= 'z')) {
+
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+                
+            }
+        }
+        System.out.println("String: "+str);
+        System.out.println("Vowels: "+ vowels);
+        System.out.println("Consonants: "+ consonants);
+    }
+
+}
