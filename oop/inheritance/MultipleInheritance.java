00155537008864df4d8af2ab15ed5beb85c7c1d3
devprajwalgotnochill <devprajwolgotnochill@gmail.com>
2026-06-19 08:48:43 +0545

    interface and multilevel inheritance

--- oop/inheritance/MultipleInheritance.java
+++ oop/inheritance/MultipleInheritance.java
@@ -1,44 +1,34 @@
-
 /*
 Java does not support multiple inheritances with classes
  */
-// Superclass
-class Vehicle {
-    void vehicleType() {
-        System.out.println("This is a Vehicle");
+// it can be achieved using interface
+//
+//
+
+public interface Father {
+    void showFather();
+}
+
+public interface Mother {
+    void showMother();
+}
+
+public class Child implements Father, Mother {
+
+    public void showFather() {
+        System.out.println("FAther");
+    }
+
+    public void showMother() {
+        System.out.println("MOther");
     }
 }
 
-// Interface for fare
-interface Fare {
-    default void fareInfo() {
-        System.out.println("Fare information");
-    }
-}
+public class MultipleInheritance {
 
-// Subclass 1: Single inheritance + multilevel
-class Car extends Vehicle {
-    void carType() {
-        System.out.println("This is a Car");
-    }
-}
-
-// Subclass 2: Hybrid inheritance (extends class + implements interface)
-class Bus extends Vehicle implements Fare {
-    void busType() {
-        System.out.println("This is a Bus");
-    }
-}
-
-public class MultipleInheritance{
     public static void main(String[] args) {
-        Car car = new Car();
-        car.vehicleType(); // inherited from Vehicle
-        car.carType();     // specific to Car
-
-        Bus bus = new Bus();
-        bus.vehicleType(); // inherited from Vehicle
-        bus.busType();     // specific to Bus
-        bus.fareInfo();    // from Fare interface
+        Child c = new Child();
+        c.showFather();
+        c.showMother();
     }
 }
